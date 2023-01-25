f099a360a9f8daecdd944d55244319f1aa2ad9d5
matt190589 <matthew.doherty97@gmail.com>
2023-01-25 13:09:27 +0000

    Data and images added. Some minor CSS added.

--- bae/src/components/Result/Result.js
+++ bae/src/components/Result/Result.js
@@ -1,31 +1,24 @@
 import React from "react";
 import "../../App.css";
-// import 'data' from '../'
+import data from "../../lib/data.js";
+
+// replace the [number] with the return of the input submission
 
 function Result() {
+  console.log(data);
   return (
-    <article class="card">
+    <div className="card">
       <header>
-        <h2>Banana</h2>
+        <h1>{data[0].food}</h1>
       </header>
-      <img
-        src="https://en.wikipedia.org/wiki/Banana_equivalent_dose#/media/File:Banana-Single.jpg"
-        alt="Hot air balloons"
-      />
-      <div class="content">
-        <p>
-          {" "}
-          480g CO2e per kilo. Bananas are a great food for anyone who cares
-          about their carbon footprint. For just 80g of CO2e you get a whole lot
-          of nutrition: 140 calories as well as stacks of vitamin C, vitamin B6,
-          potassium and dietary fibre. All in all, a fantastic component of a
-          low-carbon diet{" "}
-        </p>
+      <img src={data[0].image} alt="food picture" />
+      <div className="content">
+        <p> {data[0].description}</p>
       </div>
       <div>
-        <h1>Score: 2</h1>
+        <h1>Score: {data[0].carbonScore}</h1>
       </div>
-    </article>
+    </div>
   );
 }
 
---
+++ bae/src/lib/data.js
@@ -0,0 +1,38 @@
+const data = [
+  {
+    id: 1,
+    food: "banana",
+    image: "/Images/banana.png",
+    description:
+      " 480g CO2e per kilo. Bananas are a great food for anyone who cares about their carbon footprint. For just 80g of CO2e you get stacks of vitamins and potassium. Fantastic for a low-carbon diet.",
+    carbonScore: 2,
+  },
+
+  {
+    id: 2,
+    food: "beef",
+    image: "../../public/Images/ox.png",
+    description:
+      " One serving of Beef (100g) is equivalent to 8.46m3 of CO2 Gas. Beef is one of the highest carbon footprints of any food. To lower your impact, we suggest replacing it with chicken or non-meat alternatives",
+    carbonScore: 10,
+  },
+  {
+    id: 3,
+    food: "broccoli",
+    image: "/Images/broccoli.png",
+    description:
+      " Broccoli has a relatively low carbon footprint compared to other foods and it's great for your health. It takes around 2.0 CO2e to produce 1 kilogram.",
+    carbonScore: 1,
+  },
+
+  {
+    id: 4,
+    food: "chocolate",
+    image: "/Images/chocolate.png",
+    description:
+      " It's all in the hips and unfortunately the forests.Chocolate scores really badly for land use change because demand for cocoa has been rising for decades. Keep chocolate for your treat day",
+    carbonScore: 8,
+  },
+];
+
+export default data;
