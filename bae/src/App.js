87fa41ab6c35a7d1ce8a60301b7effb64e2cfe81
Julio Camargo <jcamargo@hotmail.co.uk>
2023-01-25 13:55:06 +0000

    setFood done

--- bae/src/App.js
+++ bae/src/App.js
@@ -1,18 +1,28 @@
 
 import Search from "./components/Search/Search";
 import Result from "./components/Result/Result";
-
+import { useState } from "react";
+import data from "./lib/data";
 import './App.css';
 import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import Page from './components/page/Page.js';
 
 function App() {
+
+  const [food, setFood] = useState("");
+  const foodByName = (name) => {
+  setFood(data.filter((food) => food.name.toLowerCase().includes(name.toLowerCase())));
+  return foodByName;
+  };
+  
+
+
   return (
     <ChakraProvider>
     <div className="App">
       <Search/>
-      <Result />
+      <Result food={foodByName}/>
     </div>
     <Page/>
     </ChakraProvider>
